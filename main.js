377c985b80e52ea7550759adff7e707599870a48
vLongas <longas.valentin@gmail.com>
2022-07-07 15:47:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+for (let i = 0; i <= 6; i++) {
+
+    
+    if (i <= 1) {
+        continue;
+    }
+    console.log(i + " caramelos")
+}
+
+
+// dejo otro ejercicio pero con while //
+let dinero = 0
+while (dinero < 1000) {
+    dinero++;
+    
+    if (dinero == 15) {
+        break;
+    }
+    console.log(dinero + " peso/s");
+}
