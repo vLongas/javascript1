9bfa36a38ad17bd53f50aef9d1b71fb658ee7921
vLongas <longas.valentin@gmail.com>
2022-07-12 22:52:52 -0300

    simulador interactivo

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-for (let i = 0; i <= 6; i++) {
+// Calcular pagos en cuotas sobre un monto determinado
+let monto= prompt('Ingrese el monto:');
 
-    
-    if (i <= 1) {
-        continue;
-    }
-    console.log(i + " caramelos")
+let cuotas= prompt('Ingrese las cuotas que desea:');
+
+CalculoDePago(monto,cuotas);
+
+function CalculoDePago(monto, cuotas){
+    let calculo= monto/cuotas;
+    alert('El pago es en '+ cuotas + ' cuotas sin interes de '+ calculo);
 }
 
+// Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
 
-// dejo otro ejercicio pero con while //
-let dinero = 0
-while (dinero < 1000) {
-    dinero++;
-    
-    if (dinero == 15) {
-        break;
+alert('Producto "1"= 1000. Producto "2"=1200. Producto "3"= 1400 ');
+
+let producto=prompt('Elija un prodcuto:');
+
+CalculoValorFinal(producto);
+
+function CalculoValorFinal(prodcuto){
+    if(prodcuto==1){
+        let impuestos= 100;
+        let descuentos= 200;
+        let valorFinal= 1000 + impuestos -descuentos;
+
+        alert('El impuesto tiene un valor de impuesto '+ impuestos+' pesos, y un descuento de ' + descuentos + ', por lo tanto el total es de '+valorFinal+ ' pesos finales');
+    }else if(prodcuto==2){
+        let impuestos= 400;
+        let descuentos= 600;
+        let valorFinal= 1200 + impuestos -descuentos;
+
+        alert('El impuesto tiene un valor de impuesto'+ impuestos+' pesos, y un descuento de ' + descuentos + ', por lo tanto el total es de '+valorFinal+ ' pesos finales');
+    }else if(prodcuto==3){
+        let impuestos= 200;
+        let descuentos= 500;
+        let valorFinal= 1400 + impuestos -descuentos;
+
+        alert('El impuesto tiene un valor de impuesto'+ impuestos+' pesos, y un descuento de ' + descuentos + ', por lo tanto el total es de '+valorFinal+ ' pesos finales');
+    }else{
+        alert('PRODUCTO NO EXISTENTE ELIJA OTRO.');
     }
-    console.log(dinero + " peso/s");
+
+
 }
